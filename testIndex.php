254f7c69c9348503d885a4c18a62a125c8a9acbb
Saad <Saad@Acecom>
2016-06-02 13:40:23 +0500

    Tested the functions of functions.php using include

--- testIndex.php
+++ testIndex.php
@@ -8,14 +8,34 @@
 
 /**
  * Description of testIndex
- *
+ * Using Require/Include and Doing the testing
  * @author Saad
  */
+
+include 'functions.php';
 class testIndex extends PHPUnit_Framework_TestCase{
     //put your code here
     public function testgarbage()
     {
         $a = 'Hello';
         $this->assertEquals('Hello',$a);
+    }
+    /**
+     * Description: testing function addnumbers
+     */
+    public function testaddnumbers()
+    {
+        $no1=3;
+        $no2=1;
+        $res=addnumbers($no1, $no2);
+        $this->assertEquals(3,$res);
+    }
+    /**
+     * Description: testing function addintstring
+     */
+    public function testaddintstring()
+    {
+        $res=addintstring();
+        $this->assertEquals('420 is not stupid',$res);
     }
 }
