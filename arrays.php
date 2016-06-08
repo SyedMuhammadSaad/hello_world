3d0e9a2fc60c2cdf03847b1dc53d1d9f78b809f7
Saad <Saad@Acecom>
2016-06-08 12:47:36 +0500

    Multidimensional and dynamic arrays.

---
+++ arrays.php
@@ -0,0 +1,18 @@
+<?php
+
+$array = array("red","green","blue");
+array_push($array, "black");
+var_dump($array);
+$array[3]="white";
+echo "<br>";
+var_dump($array);
+array_push($array, "black");//pushing in array
+echo "<br>";
+var_dump($array);
+array_pop($array);//pop last item
+echo "<br>";
+var_dump($array);
+$multarray = array(array("laptop","mobile"),array("sword","shield"));//multidimensional array
+echo "<br>";
+var_dump($multarray);
+?>
